58941288f62626ee648cb421b6bfb58e8cd6a760
ilhanTabak <herr.ilhantabak@gmail.com>
2023-02-25 13:12:03 +0100

    oufladen

--- src/Test/Test6.java
+++ src/Test/Test6.java
@@ -3,6 +3,9 @@ package Test;
 public class Test6 {
     public static void main(String[] args) {
         System.out.println("test6");
+        System.out.println("ne haber");
+        System.out.println("bu nasil is");
+
 
     }
 }
